a42bea6f0af7343af0b8b5d695a99942e317dc4a
Victor Pla <vplasanchis@gmail.com>
2022-02-01 13:25:22 +0100

    Console Class

---
+++ GameEngine/src/Log/Console.cpp
@@ -0,0 +1,40 @@
+#include "Console.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+
+namespace vge {
+
+	std::map<Console::COLOR, std::string> Console::COLOR_VALUE = {
+		{Console::RED, "\033[31m"},
+		{Console::MAGENTA, "\033[35m"},
+		{Console::BLUE, "\033[34m"},
+		{Console::GREEN, "\033[32m"},
+		{Console::WHITE, "\033[37m"},
+		{Console::CYAN, "\033[36m"},
+		{Console::YELLOW, "\033[33m"},
+	};
+	std::map<Console::SENDER, std::string> Console::SENDER_VALUE = {
+		{Console::APPLICATION, "APP"},
+		{Console::GRAPHICS_ENGINE, "GEL"},
+		{Console::INPUT_SYSTEM, "ISL"},
+		{Console::AUXILIAR, "AUX"},
+	};
+
+	Console::Console()
+	{
+	}
+
+	Console::~Console()
+	{
+	}
+
+	void Console::debug(std::string message, COLOR color, SENDER sender = Console::SENDER::AUXILIAR)
+	{
+		double currentTime = glfwGetTime();
+		std::cout << COLOR_VALUE[color] << "["+std::to_string(currentTime)+"s] " << SENDER_VALUE[sender] + ": " + message + COLOR_VALUE[Console::WHITE] << std::endl;
+	}
+
+
+
+}
---
+++ GameEngine/src/Log/Console.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <map>
+#include <string>
+
+namespace vge {
+
+	class Console
+	{
+
+	public:
+
+		Console();
+		~Console();
+
+		enum COLOR {
+			RED,
+			MAGENTA,
+			CYAN,
+			BLUE,
+			GREEN,
+			YELLOW,
+			WHITE,
+		};
+
+		enum SENDER {
+			APPLICATION,
+			GRAPHICS_ENGINE,
+			INPUT_SYSTEM,
+			AUXILIAR
+		};
+
+		static std::map<Console::COLOR, std::string> COLOR_VALUE;
+		static std::map<Console::SENDER, std::string> SENDER_VALUE;
+
+		static void debug(std::string message, COLOR color, SENDER sender);
+
+	};
+
+
+}
+
+
